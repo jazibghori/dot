2ee068b300582196b33e2ac995331f9ee3d38472
jazibghori <77227927+jazibghori@users.noreply.github.com>
2021-01-10 11:43:46 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let number = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 1) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # . . . #
+        . . . . .
+        # . . . #
+        `)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+})
